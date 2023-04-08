954cead8dee9789c716066c3f43073a75f6463ef
SHarsheev <51026122+sharsheevnursultan@users.noreply.github.com>
2023-04-08 12:51:21 +0600

    init tabs

--- lib/main.dart
+++ lib/main.dart
@@ -67,66 +67,119 @@ class _LifeTrackerState extends State<LifeTracker> {
     }
     double percentComplete = _ageInSeconds / (80 * 365 * 24 * 60 * 60);
     int percentCompleteRounded = (percentComplete * 100).round();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Life Tracker'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: const Text('Select your birthdate'),
-                  onPressed: () async {
-                    DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime.now(),
-                    );
-                    _updateAgeInOtherUnits(selectedDate!);
-                  },
-                ),
-                const SizedBox(height: 20),
-                Text(
-                    style: const TextStyle(fontSize: 18.0),
-                    'Your age in months: $_ageInMonths'),
-                const SizedBox(height: 20),
-                Text(
-                    style: const TextStyle(fontSize: 18.0),
-                    'Your age in weeks: $_ageInWeeks'),
-                const SizedBox(height: 20),
-                Text(
-                    style: const TextStyle(fontSize: 18.0),
-                    'Your age in days: $_ageInDays'),
-                const SizedBox(height: 20),
-                Text(
-                    style: const TextStyle(fontSize: 18.0),
-                    'Your age in minutes: $_ageInMinutes'),
-                const SizedBox(height: 20),
-                Text(
-                    style: const TextStyle(fontSize: 18.0),
-                    'Your age in seconds: $_ageInSeconds'),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: GridView.count(
-                    crossAxisCount: 24,
-                    children: squares,
-                  ),
-                ),
-              const SizedBox(height: 20),
-              Text(
-                  style: const TextStyle(fontSize: 18.0),
-                  '$percentCompleteRounded% of average life expectancy'),
-              const SizedBox(height: 10),
-              LinearProgressIndicator(value: percentComplete),
+
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Life Tracker'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.access_time),
+              ),
+              Tab(
+                icon: Icon(Icons.grid_view_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.percent),
+              ),
             ],
           ),
         ),
-      ), ),
+        body: TabBarView(
+          children: 
+          <Widget>[
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        child: const Text('Select your birthdate'),
+                        onPressed: () async {
+                          DateTime? selectedDate = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(1900),
+                            lastDate: DateTime.now(),
+                          );
+                          _updateAgeInOtherUnits(selectedDate!);
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                          style: const TextStyle(fontSize: 18.0),
+                          'Your age in months: $_ageInMonths'),
+                      const SizedBox(height: 20),
+                      Text(
+                          style: const TextStyle(fontSize: 18.0),
+                          'Your age in weeks: $_ageInWeeks'),
+                      const SizedBox(height: 20),
+                      Text(
+                          style: const TextStyle(fontSize: 18.0),
+                          'Your age in days: $_ageInDays'),
+                      const SizedBox(height: 20),
+                      Text(
+                          style: const TextStyle(fontSize: 18.0),
+                          'Your age in minutes: $_ageInMinutes'),
+                      const SizedBox(height: 20),
+                      Text(
+                          style: const TextStyle(fontSize: 18.0),
+                          'Your age in seconds: $_ageInSeconds'),
+                      const SizedBox(height: 20),
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      Expanded(
+                        child: GridView.count(
+                          crossAxisCount: 36,
+                          children: squares,
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      Text(
+                          style: const TextStyle(fontSize: 18.0),
+                          '$percentCompleteRounded% of average life expectancy'),
+                      const SizedBox(height: 10),
+                      LinearProgressIndicator(value: percentComplete,minHeight: 20,),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+
+        ),
+      ),
     );
   }
 }
